9c7508dc16f0c241b7ed4ea3f1f46e7f6da6983a
maph maphskiy <maphskiy@gmail.com>
2018-01-26 11:59:31 +0300

    !

--- src/services/data/operation-service.ts
+++ src/services/data/operation-service.ts
@@ -2,8 +2,8 @@ import { AxiosPromise } from "axios";
 import { OperationResult, Operation } from "../../types/operation/operation";
 
 export interface IOperationService {
-    rawGet<T>(url: string): AxiosPromise<OperationResult<T>>;
-    rawPost<T>(url: string, data: any): AxiosPromise<OperationResult<T>>
-    rawPut<T>(url: string, data: any): AxiosPromise<Operation>
-    rawDelete<T>(url: string): AxiosPromise<Operation>
+    get<T>(url: string): AxiosPromise<OperationResult<T>>;
+    post<T>(url: string, data: any): AxiosPromise<OperationResult<T>>
+    put<T>(url: string, data: any): AxiosPromise<Operation>
+    delete<T>(url: string): AxiosPromise<Operation>
 }
