1be53db4b748fc239606e511f47a244e6aabf62d
maph maphskiy <maphskiy@gmail.com>
2018-01-29 08:31:42 +0300

    added async operations to IOperationService

--- src/services/data/operation-service.ts
+++ src/services/data/operation-service.ts
@@ -3,7 +3,11 @@ import { OperationResult, Operation } from "../../types/operation/operation";
 
 export interface IOperationService {
     get<T>(url: string): AxiosPromise<OperationResult<T>>;
-    post<T>(url: string, data: any): AxiosPromise<OperationResult<T>>
-    put<T>(url: string, data: any): AxiosPromise<Operation>
-    delete<T>(url: string): AxiosPromise<Operation>
+    post<T>(url: string, data: any): AxiosPromise<OperationResult<T>>;
+    put<T>(url: string, data: any): AxiosPromise<Operation>;
+    delete<T>(url: string): AxiosPromise<Operation>;
+    getAsync<T>(url: string): OperationResult<T>;
+    postAsync<T>(url: string, data: any): OperationResult<T>;
+    putAsync<T>(url: string, data: any): Operation;
+    deleteAsync<T>(url: string): Operation;
 }
