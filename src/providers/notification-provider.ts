7f78492832e70094aa2fba3f703c919baf604787
maph maphskiy <maphskiy@gmail.com>
2018-04-18 14:27:23 +0300

    added notification provider interface

---
+++ src/providers/notification-provider.ts
@@ -0,0 +1,11 @@
+import { NotificationItem } from "../types/notification-item/notification-item";
+
+export interface INotificationProvider {
+    notifications: NotificationItem[];
+    Error(message?: any, title?: string, dismissAfter?: number): void;
+    Success(message?: any, title?: string, dismissAfter?: number): void;
+    Info(message?: any, title?: string, dismissAfter?: number): void;
+    Warning(message?: any, title?: string, dismissAfter?: number): void;
+    Debug(message?: any): void;
+    GetNotifications(): NotificationItem[];
+}
