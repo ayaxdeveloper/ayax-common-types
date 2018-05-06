9eee2526390065474b441a9e891b09a10fcc6e68
maph maphskiy <maphskiy@gmail.com>
2018-05-06 08:35:23 +0300

    CheckPageIsOutOfRange

--- src/types/pagination/pagination.ts
+++ src/types/pagination/pagination.ts
@@ -20,7 +20,7 @@ export class Pagination implements IPagination {
     }
 
     public CheckPageIsOutOfRange(): boolean {
-        if(this.page * this.rowsPerPage > this.totalItems) {
+        if(this.page != 1 && this.page * this.rowsPerPage > this.totalItems) {
             this.page = 1;
             return true;
         }
