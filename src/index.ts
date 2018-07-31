0d23957c3a4593ea91bc115fb0e7f696511c8a3c
maph maphskiy <maphskiy@gmail.com>
2018-07-31 14:44:49 +0300

    Добавил ICrudService

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./Types";
 export * from "./Settings";
 export * from "./Providers/INotificationProvider";
 export * from "./services/IHttpService";
+export * from "./services/ICrudService";
