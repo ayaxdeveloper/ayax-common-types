a3be4c7b12329776298db71909073db2ed335af5
karnaukhov_vo <karnaukhov_vo@ayax.ru>
2018-10-03 10:56:03 +0300

    Поправил TResult для ISearchableService

--- src/Services/ISearchableService.ts
+++ src/Services/ISearchableService.ts
@@ -1,3 +1,5 @@
+import { SearchResponse } from "../Types/SearchResponse/SearchResponse";
+
 export interface ISearchableService<TRequest, TResult> {
-    Search(request?: TRequest): Promise<TResult[]>;
+    Search(request?: TRequest): Promise<SearchResponse<TResult[]>>;
 }
