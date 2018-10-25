4ceb4e4fc5554d549816f731d643af47b4327475
Толубаев Александр А <tolubaev_aa@ajax.ru>
2018-10-25 14:56:13 +0300

    click notification

--- src/Providers/INotificationProvider.ts
+++ src/Providers/INotificationProvider.ts
@@ -2,10 +2,22 @@ import { NotificationItem } from "../Types/NotificationItem/NotificationItem";
 
 export interface INotificationProvider {
     notifications: NotificationItem[];
-    Error(message?: any, title?: string, dismissAfter?: number): void;
-    Success(message?: any, title?: string, dismissAfter?: number): void;
-    Info(message?: any, title?: string, dismissAfter?: number): void;
-    Warning(message?: any, title?: string, dismissAfter?: number): void;
+    Error(message?: any, title?: string, dismissAfter?: number, onClick?: () => void, closeAfterClick?: boolean): void;
+    Success(
+        message?: any,
+        title?: string,
+        dismissAfter?: number,
+        onClick?: () => void,
+        closeAfterClick?: boolean,
+    ): void;
+    Info(message?: any, title?: string, dismissAfter?: number, onClick?: () => void, closeAfterClick?: boolean): void;
+    Warning(
+        message?: any,
+        title?: string,
+        dismissAfter?: number,
+        onClick?: () => void,
+        closeAfterClick?: boolean,
+    ): void;
     Debug(message?: any): void;
     GetNotifications(): NotificationItem[];
 }
--- src/Types/NotificationItem/NotificationItem.ts
+++ src/Types/NotificationItem/NotificationItem.ts
@@ -9,14 +9,24 @@ export class NotificationItem {
     icon: string;
     showing: boolean;
     outline: boolean;
-    public constructor(message: any, icon: string, color: string, title: string, dismissAfter: number) {
+    onClick: () => void;
+
+    public constructor(
+        message: any,
+        icon: string,
+        color: string,
+        title: string,
+        dismissAfter: number,
+        onClick?: () => void,
+    ) {
         this.id = Guid.newGuid().toString();
         this.showing = true;
         this.outline = false;
         this.message = message;
-        this.icon = icon; 
+        this.icon = icon;
         this.color = color;
         this.dismissAfter = dismissAfter;
         this.title = title;
+        this.onClick = onClick;
     }
 }
