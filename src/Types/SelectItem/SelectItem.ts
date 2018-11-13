3b45942f398f0e4a697f53f27237fb40c007fec2
Толубаев Александр А <tolubaev_aa@ajax.ru>
2018-11-13 15:17:47 +0300

    new selectItem property

--- src/Types/SelectItem/SelectItem.ts
+++ src/Types/SelectItem/SelectItem.ts
@@ -5,6 +5,7 @@ export class SelectItem {
     text: string;
     disabled = false;
     selected = false;
+    filters: Array<{filterName: string, filterValue: any}>;
     constructor(init?: Partial<SelectItem>) {
         if (init) {
             Object.assign(this, init);
