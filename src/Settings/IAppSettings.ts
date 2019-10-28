0b541269c5f0cb03c4a3a685cbb75d92fed01657
krikun_sa <krikun_sa@ayax.ru>
2019-10-28 10:49:28 +0300

    AS-1664 сохранения настроек url v.3.1.11

--- src/Settings/IAppSettings.ts
+++ src/Settings/IAppSettings.ts
@@ -1,10 +1,12 @@
 import { IClientSettings } from "./IClientSettings";
 import { INotificationSettings } from "./INotificationSettings";
 import { IServerSettings } from "./IServerSettings";
+import { UrlsSettings } from "./UrlsSettings";
 
 export interface IAppSettings {
     Server(): IServerSettings;
     Client(): IClientSettings;
     Notification(): INotificationSettings;
-    Custom(): {[name: string] : any};
+    Custom(): { [name: string]: any };
+    getUrlsSettings(): Promise<UrlsSettings>
 }
